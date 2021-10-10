5377d18330e7b609e0feba706af79239d8c16a07
hoangSnapeV <hoangvu01639337130@gmail.com>
2021-10-10 09:51:50 +0700

    ok

---
+++ pointer_test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+struct HocSinh {
+   char * name;
+   float math_grade;
+   float english_grade;
+   float literature_grade;
+
+};
+
+int main()
+{
+    struct HocSinh student_1;
+    student_1.name = "Hoang Vu";
+    student_1.math_grade = 9;
+    student_1.english_grade = 9.5;
+    student_1.literature_grade = 10;
+    //
+    struct HocSinh student_2;
+    student_2.name = "Tran Cong Lam";
+    student_2.math_grade = 7;
+    student_2.english_grade = 6.5;
+    student_2.literature_grade = 6.6;
+    //
+    struct HocSinh student_3;
+    student_3.name = "Tong Tran Le Huy";
+    student_3.math_grade = 1.47;
+    student_3.english_grade = 1.19;
+    student_3.literature_grade = 3;
+    printf("name: %s math_grade: %f english_grade: %f literature_grade: %f\n", student_1.name , student_1.math_grade, student_1.english_grade, student_1.literature_grade); 
+    printf("name: %s math_grade: %f english_grade: %f literature_grade: %f\n", student_2.name , student_2.math_grade, student_2.english_grade, student_2.literature_grade); 
+    printf("name: %s math_grade: %f english_grade: %f literature_grade: %f\n", student_3.name , student_3.math_grade, student_3.english_grade, student_3.literature_grade);
+    ///
+    printf("\n");
+    struct HocSinh duong_class[3] = {
+        student_1,
+        student_2,
+        student_3
+    };
+    for(int i = 0; i < 3; i++) 
+    {
+        struct HocSinh student = duong_class[i];
+        printf("name= %s, math_grade= %f, english_grade= %f, literature_grade= %f\n", student.name, student.math_grade, student.english_grade,student.literature_grade);
+    }
+}

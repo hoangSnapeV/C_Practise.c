7f163363c94f12af2c92fe8a1a95c3bd10dbdec0
hoangSnapeV <hoangvu01639337130@gmail.com>
2021-10-16 19:53:56 +0700

    oke

---
+++ convert.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define dam 1609
+
+int main() 
+{
+    int num = 1;
+    scanf("%d", &num);
+    float result = num * dam;
+    printf("Result = %f", result);
+   //return 0;
+}
